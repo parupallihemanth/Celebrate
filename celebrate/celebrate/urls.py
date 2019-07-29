368227934c0e055757383b757d0503e280adc389
parupallihemanth <babuparupalli@gmail.com>
2019-07-29 14:56:57 -0700

    register page started

--- celebrate/celebrate/urls.py
+++ celebrate/celebrate/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('main/', include('celebrateapp1.urls'))
+    path('main/', include('celebrateapp1.urls')),
     path('register/',include('register.urls'))
 ]
